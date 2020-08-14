5f67d4a8518aeb1533404844f7c13862c97570de
Rojin Abraham <rojinabraham@gmail.com>
2020-08-14 19:18:49 +0530

    Updates

---
+++ precache-manifest.692b6c144a67ecdaca38a69cef927fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516c3f324aaa56c99c445bcb68a8b51d",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "cc5c1e07b2cff4697923",
+    "url": "/dad-jokes/static/css/main.262a0d24.chunk.css"
+  },
+  {
+    "revision": "2ee2c8742dfdb946aadf",
+    "url": "/dad-jokes/static/js/2.22d27992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.22d27992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5c1e07b2cff4697923",
+    "url": "/dad-jokes/static/js/main.ca43af8d.chunk.js"
+  },
+  {
+    "revision": "0ee283f3a45f81336623",
+    "url": "/dad-jokes/static/js/runtime-main.751de3d0.js"
+  }
+]);
